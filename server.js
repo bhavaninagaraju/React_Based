f524af6d09bdee9bb4bc2d1694cf8f62b03c3302
Bhavani Nagaraju <73734117+bhavaninagaraju@users.noreply.github.com>
2026-02-11 23:34:19 +0530

    Fix user structure

--- server.js
+++ server.js
@@ -16,8 +16,16 @@ app.get("/", (req, res) => {
 });
 app.get("/users", (req, res) => {
     res.json([
-      { id: 1, name: "Alice" },
-      { id: 2, name: "Bob" }
+      { id: 1,
+        firstName: "Alice",
+        lastName: "Johnson",
+        dateOfBirth: "1995-06-15",
+        email: "alice@example.com" },
+      { id: 2,
+        firstName: "Bob",
+        lastName: "Smith",
+        dateOfBirth: "1992-09-20",
+        email: "bob@example.com" }
     ]);
 });
 
