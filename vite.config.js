a12bb74c9dab001aeb98b01406d4592728e47dee
Bhavani Nagaraju <73734117+bhavaninagaraju@users.noreply.github.com>
2026-02-12 00:28:03 +0530

    Fix build config and backend setup

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // <--- this must match your repo name
+  base: '/React_Based/',
+ // <--- this must match your repo name
   build: {
     outDir: 'dist'
   }
